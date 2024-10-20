5ad957068485e4d8d83795ee94980ecfded2f0ff
Colton English <colton@sparksis.com>
2024-10-20 13:10:18 +0000

    fix all the things!

--- scripts/mkdocs_build.sh
+++ scripts/mkdocs_build.sh
@@ -4,7 +4,7 @@
 pip install mkdocs mkdocs-material
 
 # Convert RST files to Markdown using pandoc
-find . -name "*.rst" -exec sh -c 'pandoc "$0" -o "${0%.rst}.md"' {} \;
+find . -name "*.rst" -exec sh -c 'pandoc "$0" -o "docs/${0%.rst}.md"' {} \;
 
 # Build the documentation using mkdocs
-mkdocs build 
+mkdocs build
