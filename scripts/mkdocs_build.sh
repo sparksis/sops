f93f301d5d17a66fd3a3ad50a8ccc53214453b75
Colton English <colton@sparksis.com>
2024-10-20 14:08:05 +0000

    yeet

--- scripts/mkdocs_build.sh
+++ scripts/mkdocs_build.sh
@@ -4,13 +4,13 @@ set -x
 
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3-$(uname)-$(uname -m).sh -bu
-./miniforge3/bin/conda install -c conda-forge pandoc
+${HOME}/miniforge3/bin/conda install -c conda-forge pandoc
 
 # Install mkdocs and mkdocs-material
 pip install mkdocs mkdocs-material
 
 # Convert RST files to Markdown using pandoc
-find . -name "*.rst" -exec sh -c './miniforge3/bin/pandoc "$0" -o "docs/${0%.rst}.md"' {} \;
+find . -name "*.rst" -exec sh -c '${HOME}/miniforge3/bin/pandoc "$0" -o "docs/${0%.rst}.md"' {} \;
 
 # Build the documentation using mkdocs
 mkdocs build
