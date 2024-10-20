4ff0be326984fc5cbb42fe49d14071c1be1c6909
Colton English <colton@sparksis.com>
2024-10-20 13:36:40 +0000

    install conda

--- scripts/mkdocs_build.sh
+++ scripts/mkdocs_build.sh
@@ -2,6 +2,11 @@
 
 set -x
 
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+
 conda install -c conda-forge pandoc
 
 # Install mkdocs and mkdocs-material
