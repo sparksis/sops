5e5308c958f990eb448f5084bc2209f0d9a32c50
Colton English <colton@sparksis.com>
2024-10-20 17:11:47 +0300

    Convert docs.yml to build using mkdocs (#1)
    Publish the readme using mkdocs

---
+++ scripts/mkdocs_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3-$(uname)-$(uname -m).sh -bu
+${HOME}/miniforge3/bin/conda install -c conda-forge pandoc
+
+# Install mkdocs and mkdocs-material
+pip install mkdocs mkdocs-material
+
+# Convert RST files to Markdown using pandoc
+find . -name "*.rst" -exec sh -c '${HOME}/miniforge3/bin/pandoc "$0" -o "docs/${0%.rst}.md"' {} \;
+
+# Build the documentation using mkdocs
+mkdocs build
